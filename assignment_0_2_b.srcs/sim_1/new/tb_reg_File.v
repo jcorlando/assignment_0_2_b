882f1063cdb1784b26e515c9a71f18dbeed6e7e5
Juan Orlando <jcorlandomc@yahoo.com>
2020-08-21 23:22:15 -0700

    8/21/2020 11:21 pm

---
+++ assignment_0_2_b.srcs/sim_1/new/tb_reg_File.v
@@ -0,0 +1,86 @@
+`timescale 1ns / 1ps
+
+module tb_reg_File;
+
+//Inputs
+reg CLK;
+reg RFWE;
+reg [4:0] RFR1;
+reg [4:0] RFR2;
+reg [4:0] RFWA;
+reg [31:0] RFWD;
+//Outputs
+wire [31:0] RFRD1;
+wire [31:0] RFRD2;
+// Clock generation 
+initial
+begin
+    CLK <= 0;
+    RFWE <= 1;
+    RFR1 <= 0;
+    RFR2 <= 0;
+    RFWA <= 0;
+    RFWD <= 0;
+    
+end
+always #50 CLK <= ~CLK;
+//Instantiate DUT
+reg_File DUT( .CLK(CLK), .RFWE(RFWE), .RFR1(RFR1), 
+                .RFR2(RFR2), .RFWA(RFWA), .RFWD(RFWD), 
+                    .RFRD1(RFRD1), .RFRD2(RFRD2) );
+
+initial
+begin
+    @(posedge CLK);
+    @(posedge CLK);
+    RFR1 <= 0;
+    RFR2 <= 0;
+    RFWE <= 1;
+    RFWA <= 0;
+    RFWD <= 32'hffffffff;
+    @(posedge CLK);
+    RFR1 <= 1;
+    RFR2 <= 1;
+    RFWE <= 1;
+    RFWA <= 1;
+    RFWD <= 32'h11111111;
+    @(posedge CLK);
+    RFR1 <= 2;
+    RFR2 <= 2;
+    RFWE <= 1;
+    RFWA <= 2;
+    RFWD <= 32'haaaaaaaa;
+    @(posedge CLK);
+    RFR1 <= 3;
+    RFR2 <= 3;
+    RFWE <= 1;
+    RFWA <= 3;
+    RFWD <= 32'heeeeeeee;
+    @(posedge CLK);
+    RFR1 <= 4;
+    RFR2 <= 4;
+    RFWE <= 1;
+    RFWA <= 4;
+    RFWD <= 32'h99999999;
+    @(posedge CLK);
+    RFR1 <= 5;
+    RFR2 <= 5;
+    RFWE <= 1;
+    RFWA <= 5;
+    RFWD <= 32'h66666666;
+    @(posedge CLK);
+    RFR1 <= 6;
+    RFR2 <= 6;
+    RFWE <= 1;
+    RFWA <= 6;
+    RFWD <= 32'h33333333;
+    @(posedge CLK);
+    RFR1 <= 7;
+    RFR2 <= 7;
+    RFWE <= 1;
+    RFWA <= 7;
+    RFWD <= 32'hbbbbbbbb;
+end
+
+endmodule
+
--- assignment_0_2_b.srcs/sources_1/new/reg_File.v
+++ assignment_0_2_b.srcs/sources_1/new/reg_File.v
@@ -6,13 +6,19 @@ module reg_File( input CLK,
                  input [31:0] RFWD,
                  output [31:0] RFRD1, RFRD2 );
     
-    reg [31:0] rf[31:0];
+    reg [31 : 0] rf[0 : 31];
+    reg [4:0] read_addr_1;      // Registered address for write-first mode
+    reg [4:0] read_addr_2;      // Registered address for write-first mode
     
-    always @ (posedge CLK) 
-        if (RFWE) rf[RFWA] <= RFWD;
+    always @ (posedge CLK)
+    begin
+        if (RFWE) begin rf[RFWA] <= RFWD; end
+        read_addr_1 <= RFR1;        // Registered address for write-first mode
+        read_addr_2 <= RFR2;        // Registered address for write-first mode
+    end
     
-    assign RFRD1 = (RFR1 != 0) ? rf[RFR1] : 0;
-    assign RFRD2 = (RFR2 != 0) ? rf[RFR2] : 0;
+    assign RFRD1 = rf[read_addr_1];     // Synchronous Read
+    assign RFRD2 = rf[read_addr_2];     // Synchronous Read
     
 endmodule
 
