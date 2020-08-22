b9fcb8ac4d291ca55dd50b6badf2c0d3d6886097
Juan Orlando <jcorlandomc@yahoo.com>
2020-08-21 17:20:22 -0700

    8/21/2020 5:20 pm

--- assignment_0_2_b.srcs/sources_1/new/reg_File.v
+++ assignment_0_2_b.srcs/sources_1/new/reg_File.v
@@ -1,10 +1,18 @@
 `timescale 1ns / 1ps
 
-
-module reg_File(  );
-
-
-
+module reg_File( input CLK,
+                 input RFWE,
+                 input [4:0] RFR1, RFR2, RFWA,
+                 input [31:0] RFWD,
+                 output [31:0] RFRD1, RFRD2 );
+    
+    reg [31:0] rf[31:0];
+    
+    always @ (posedge CLK) 
+        if (RFWE) rf[RFWA] <= RFWD;
+    
+    assign RFRD1 = (RFR1 != 0) ? rf[RFR1] : 0;
+    assign RFRD2 = (RFR2 != 0) ? rf[RFR2] : 0;
+    
 endmodule
-
 
